eaac956fc0c1394ddbce96b1a830fd5ea3544668
Trevor Cheung <tcheung@bayviewglen.ca>
2021-01-27 10:37:57 -0500

    Day One Homework - Display to Console

---
+++ Unit 1 - Introduction to Java/src/week1/DayOneHomework.java
@@ -0,0 +1,13 @@
+package week1;
+
+/**
+ * DayOneHomework
+ */
+public class DayOneHomework {
+    public static void main(String[] args) {
+        System.out.println("TTTTTTT     CCC  \n   T       C   C \n   T      C     C\n   T      C      \n   T      C     C\n   T       C   C \n   T        CCC  \n"); //Question 1
+        System.out.println("  /\\  \n /  \\ \n/    \\\n------\n|    |\n|    |\n|    |\n------\n"); //Question 2
+        System.out.println("Bayview Glen School Moatfield Campus\n85 Moatfield Drive\nToronto, Ontario M3B 3L6\n"); //Question 3
+        System.out.println("/////////////////////////////////////////////////////////////////////////\n/\t\t\t\t\t\t\t\t\t/\n/\t\t\tOur Lady of Mercy Hospital\t\t\t/\n/\t\t\t--------------------------\t\t\t/\n/\t\t\t\t\t\t\t\t\t/\n/\t\t\t    1306 Central City    \t\t\t/\n/\t\t\t   Phone (416) 555-1212 \t\t\t/\n/\t\t\t\t\t\t\t\t\t/\n/////////////////////////////////////////////////////////////////////////\n"); //Question 4
+    }
+}
